b88a2d4e178129b611004eec112e6a9cfbbbdb8f
Денис Марьянский <d.maryanskiy@gmail.com>
2023-05-05 14:46:27 +0300

    Updated start handler and added start service to work with SQLAlchemy and new user functionality.

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,37 +1,14 @@
-import logging
-
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from config import PROPERTIES, QUERIES
-from db.db import query_execute
+from config import PROPERTIES
 from .response import send_response
+from services.start import get_or_create_user
 
-logger = logging.getLogger(__name__)
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.message.from_user
-    user_exists = await query_execute(
-        QUERIES["GET_USER"],
-        {
-            "username": user.username
-        }
-    )
-    if not user_exists:
-        row = await query_execute(
-            QUERIES["ADD_USER"],
-            {
-                "username": user.username,
-                "firstname": user.first_name,
-                "lastname": user.last_name
-            }
-        )
-        user_id = row.first()[0]
-        await query_execute(
-            QUERIES["CREATE_BALANCE"],
-            {
-                "id": user_id
-            }
-        )
-
+async def start(update: Update,
+                context: ContextTypes.DEFAULT_TYPE,
+            ):
+    
+    await get_or_create_user(update)
+    
     await send_response(update, context, PROPERTIES["GREETINGS"])
---
+++ bot/services/start.py
@@ -0,0 +1,52 @@
+import logging
+
+from sqlalchemy.ext.asyncio import AsyncResult
+from sqlalchemy.exc import IntegrityError
+from telegram import Update
+
+from db.db import get_session, session_commit
+from db import models
+from .exceptions import HTTPException
+
+logger = logging.getLogger(__name__)
+
+async def get_or_create_user(update: Update):
+    """
+    Function checks whether user already exists in DB.
+    If not, it creates user and budget instances in DB.
+    """
+    # TODO: find a way to fix session receiving operation 
+    session = None
+    async for s in get_session():
+        session = s
+
+    user = update.message.from_user
+    user_exists_query = models.telegram_user.select().where(models.telegram_user.c.username == user.username)
+    user_result: AsyncResult = await session.execute(user_exists_query)
+    user_exists = user_result.one_or_none() # get user
+    if not user_exists:
+        user_data = {
+            "username": user.username,
+            "firstname": user.first_name,
+            "lastname": user.last_name
+        }
+
+        user_create_query = models.telegram_user.insert().values(**user_data)
+        user_result: AsyncResult = await session.execute(user_create_query) # create user
+
+        logger.info("user created")
+
+        last_user_id: int = user_result.inserted_primary_key[0]
+
+        for i in range(1, 4):
+            budget_data = {
+                "currency": i,
+                "balance": 0,
+                "telegram_user": last_user_id
+            }
+            budget_create_query = models.budget.insert().values(**budget_data) # create budget
+            await session.execute(budget_create_query)
+        
+        logger.info("budget created")
+
+        await session_commit(IntegrityError, HTTPException("user already exists in DB."), session)
